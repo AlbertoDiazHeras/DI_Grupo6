9fe813d8dd83962002b34d5ee98e1d9df0cad54a
AlbertoDiazHeras <93120404+AlbertoDiazHeras@users.noreply.github.com>
2021-10-25 13:28:16 +0200

    first commit

---
+++ database/migrations/2021_10_25_094250_create_discount_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->foreign("id")->references("discount_id")->on("companies")->onUpdate("cascade");
+            $table->string('name');
+            $table->integer('discount');
+            $table->tinyInteger('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discount');
+    }
+}
