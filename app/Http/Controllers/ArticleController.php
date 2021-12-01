1260bc3cce78fb8857343f155448e48e0fc123eb
AlbertoDiazHeras <93120404+AlbertoDiazHeras@users.noreply.github.com>
2021-12-01 23:54:19 +0100

    crear articulo y borrar corregidos

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,51 +4,82 @@ namespace almagest\Http\Controllers;
 
 use Request;
 use almagest\Article;
+use almagest\Family;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::where('deleted','===','0')->get();
             return view('admin.articulos',['articles'=>$articles]);
     }
     public function crear()
     {
-        return view('admin.crearArticulo');
-        
-        
+        $familias=Family::all();
+        return view('admin.crearArticulo',compact('familias'));
     }
 
-    public function aniadirArticulo(){
+    public function aniadirArticulo(){  
+        
         $respuestas=array();
-        $respuesta=(Request::input('name'));
         $respuestas=(Request::all());
+        //dd(Request::all());
+      $checked = (Request::has('peso/tamanio'));
+      $tama = (Request::get('name'));
+      if($checked=='value1'){
+          $peso=Request::get('peso');
+          $size=0;
+      }
+
+      if($checked=='value2'){
+        if($tama=='numero'){
+            $size=Request::get('numero');
+            $peso=0;
+        }
+        else if($tama=='valor'){
+            $size=Request::get('valor');
+            $peso=0;
+        }
+        else if($tama=='compuesto'){
+            $size=Request::get('ancho').Request::get('alto');
+            $peso=0;
+        }
+        else{
+            $size=0;
+            $peso=0;
+        }
+
+      }
+
+//$checkValue = dd(Request::get('peso/tamanio'));
+       //$checkValue = dd(Request::get('name'));
+
+        $respuesta=(Request::input('name'));
+        $familias=Family::all();
         $articulo=Article::create([
-            'name' => $respuestas['name'],
+            'name' => $respuestas['nombre'],
             'description'=> $respuestas['description'],
             'price_min'=> $respuestas['number_min'],
             'price_max'=> $respuestas['number_max'],
             'color_name'=> $respuestas['color_name'],
-            'weight'=> $respuestas['weight'],
-            'size'=> $respuestas['size'],
-            'family_id'=> $respuestas['family_id'],
+            'weight'=> $peso,
+            'size'=> $size,
+            'family_id'=> $respuestas['family'],
             'deleted'=> 0
         ]);
-        $articles = Article::all();
-        return view('admin.articulos',['articles'=>$articles]);
+        $articulo->save();
+        $articles = Article::where('deleted','===','0')->get();
+        return view('admin.articulos',compact('articles'));
     }
 
     public function borrar($id)
     {
-        Article::destroy($id);
-        $articles = Article::all();
-        return view('admin.articulos',['articles'=>$articles]);
-
-    }
-
-    public function alertaBorrar($id)     
-    {
-        return view('admin.alertaBorrar',['id'=>$id]);
+        $articulo=Article::find($id);
+        $articulo->deleted='1';
+        $articulo->save();
+        $articles = Article::where('deleted','===','0')->get();
+        return view('admin.articulos',compact('articles'));
+        
 
     }
 
